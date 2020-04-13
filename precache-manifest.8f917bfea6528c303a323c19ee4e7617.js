62dcddcf3b5ad7e489872222a2b2b1622d1f9887
unknown <felipecarvalho180@gmail.com>
2020-04-13 19:30:40 -0300

    Updates

---
+++ precache-manifest.8f917bfea6528c303a323c19ee4e7617.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fe39c83d1f1376716bd42007a33fa18",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "769fedd122a70f0376c6",
+    "url": "/resume/static/js/2.f0deed56.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/resume/static/js/2.f0deed56.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95e1c9e94d1bda7f32b2",
+    "url": "/resume/static/js/main.d124ad96.chunk.js"
+  },
+  {
+    "revision": "c3e9b6382508607b9d79",
+    "url": "/resume/static/js/runtime-main.07dda928.js"
+  },
+  {
+    "revision": "d5b93dd52d3ebe4ab7687492684cfa8e",
+    "url": "/resume/static/media/Felipe.d5b93dd5.pdf"
+  },
+  {
+    "revision": "37424448b77a5f9dd66522b4cc72b3a3",
+    "url": "/resume/static/media/ProfileImage.37424448.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/felipecarvalho180/resume/precache-manifest.b896ab9a1dee5d115ff5b51c5d1a32fb.js"
+  "/resume/precache-manifest.8f917bfea6528c303a323c19ee4e7617.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/felipecarvalho180/resume/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/resume/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
